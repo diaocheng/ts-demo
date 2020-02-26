7d327bc2c625ce00c35beec7f6631be248dd14d0
nashaofu <diaocheng@outlook.com>
2020-02-26 21:11:26 +0800

    delete 删除lombok

--- server/src/main/java/com/nashaofu/blog/model/Article.java
+++ server/src/main/java/com/nashaofu/blog/model/Article.java
@@ -1,13 +1,11 @@
 package com.nashaofu.blog.model;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Data
 @Document
 public class Article implements Serializable {
     @Id
@@ -15,4 +13,36 @@ public class Article implements Serializable {
     private String title;
     private String content;
     private LocalDateTime time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
 }
